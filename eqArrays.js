49d076d373dd920836d18e1813d652109fcea72a
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-09-25 12:19:01 -0600

    Update fix for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,21 +3,21 @@ const eqArrays = function(actual, expected) {
     return console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
  
-  for (let i = 0;  i < actual.length; i++){
-    if (Array.isArray(actual[i]) && Array.isArray(expected[i])){
-      if(!eqArrays(actual[i], expected[i])){
+  for (let i = 0;  i < actual.length; i++) {
+    if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+      if (!eqArrays(actual[i], expected[i])) {
         return console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
       }
     } else if (actual[i] !== expected[i]) {
       return console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
     }
-  } 
-    return console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  };
+  }
+  return console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+};
  
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
