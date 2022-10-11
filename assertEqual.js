69bb897f95adcdf5c1b6ec5e69f9cc3d5db002e6
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-11 16:10:18 -0600

    assert Equal Update

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
     return console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   }
   console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-};
+}
   
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
