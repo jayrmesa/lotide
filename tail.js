52d40d90ea68c8aeab6cb89e5a5442406540c933
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-11 16:54:16 -0600

    tail Update

--- tail.js
+++ tail.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 const tail = function(array) {
   return array.length > 2 ? array.slice(1) : [];
-};
+}
 
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
