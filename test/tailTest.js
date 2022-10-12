5bd8635d2dfaf7c0fcb80be506f34a4c63afd756
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-12 17:28:02 -0600

    Refractoring Using M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+describe("#tail", () => {
+  it(`returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("does not remove elements from array", function() {
+    const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+});
