28ee8d1818f800530dec204133ecf6d381a40948
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-11 19:10:21 -0600

    middle Update

--- middle.js
+++ middle.js
@@ -1,53 +1,20 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
- 
-  for (let i = 0;  i < actual.length; i++) {
-    if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
-      if (!eqArrays(actual[i], expected[i])) {
-        return false;
-      }
-    } else if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  let message = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  if (!eqArrays(actual, expected)) {
-    message = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-  
-  console.log(message);
-  return message;
-};
-
-
-const middle = function(array) {
-  
-const arr = array.length;
-  if (arr <= 2) {
+const middle = function(list) {
+  if (list.length < 3) {
     return [];
-  } else if (arr % 2 !== 0) {
-    let mid = Math.floor(arr / 2);
-    return array[mid];
-  } else {
-    let mid = (arr / 2) - 1;
-    return array.slice(mid, mid + 2);
-
   }
+
+  let mid = Math.floor(list.length / 2);
+
+  if (list.length % 2 === 0) {
+    return list.slice(mid -1, mid + 1);
+  }
+    
+  return list.slice(mid, mid + 1);
+
 };
 
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
+module.exports = middle; 
 
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
