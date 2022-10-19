fc28c0d687ed28befb68be909cbd61b69fe2e7c7
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-18 22:59:36 -0600

    Updated with some comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,6 @@
 const eqArrays = require('./eqArrays');
 
+//check if two values are actual and expected, if not assertion fails
 const assertArraysEqual = function(actual, expected) {
   let message = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   if (!eqArrays(actual, expected)) {
--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,8 @@
 const eqArrays = require('./eqArrays');
  
+// Returns true if both objects have identical keys with identical values.
+// If not then false
+
  const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
@@ -10,7 +13,7 @@ const obj1keys = Object.keys(object1);
   for (let i = 0; i < obj1keys.length; i++) {
     
     let key = obj1keys[i];
-
+//Checks if its an array
     if (Array.isArray(object1[key])) {
       return eqArrays(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
