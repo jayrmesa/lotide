12f7a2ceb8d12427881ac7b4415c7b268f11f9e3
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-24 12:50:04 -0600

    fix an issue to return false if the the same key, but different values in both objects

--- eqObjects.js
+++ eqObjects.js
@@ -3,27 +3,30 @@ const eqArrays = require('./eqArrays');
 // Returns true if both objects have identical keys with identical values.
 // If not then false
 
- const eqObjects = function(object1, object2) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
-const obj1keys = Object.keys(object1);
-
-  for (let i = 0; i < obj1keys.length; i++) {
-    
-    let key = obj1keys[i];
-//Checks if its an array
+  //Checks if its an array
+  for (const key in object1) {
     if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key], object2[key]);
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (typeof object1[key] === 'object' && object1[key] !== null) {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
     } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
 
-    return true;
-  };
-  
+  return true;
+};
 
-module.exports = eqObjects; 
+module.exports = eqObjects;
+
+
 
